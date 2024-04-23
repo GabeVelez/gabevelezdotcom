efba91f83cc7ad1e090811a87c7d9d027dc40e81
Gabriel Velez <gabe@designoncue.com>
2024-04-23 03:50:59 -0400

    adding sticky header gradient on scroll and making some css more responsive

--- layout.js
+++ layout.js
@@ -1,4 +1,12 @@
 $(function() {
     $("header").load("layout/nav.html");
     $("footer").load("layout/footer.html");
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > 50) {
+            $("header").addClass("header__active");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $("header").removeClass("header__active");
+        }
+    });
 });
