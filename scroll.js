14f0e18fe9fb28f8eb6773d7cb4a84d664d53903
Gabriel Velez <gabe@designoncue.com>
2024-04-23 05:19:17 -0400

    going down the scroll.js rabbithole

--- scroll.js
+++ scroll.js
@@ -1,3 +1,11 @@
 $(function() {
-    console.log("Scroll loaded");   
+    console.log("Scroll loaded");
+    if($(window).scrollTop() > 50) {
+        $(".navbar").addClass("nav__active");
+        console.log("nav__active class added");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".navbar").removeClass("nav__active");
+       console.log("nav__active class removed");
+    }
 });
