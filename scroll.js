a6776eb98ce32f8759111d30360c2e4a53b65a88
Gabriel Velez <gabe@designoncue.com>
2024-04-23 07:24:00 -0400

    reapplying scroll

--- scroll.js
+++ scroll.js
@@ -1,10 +1,13 @@
 $(function() {
+    console.log("scroll js file function started");
     $(window).on("scroll", function() {
-        console.log("scroll dude");
+        console.log("windo scroll began");
         if($(window).scrollTop() > 10) {
             $("header").addClass("nav__active");
+            console.log("added class");
         } else {
            $("header").removeClass("nav__active");
+           console.log("removed class");
         }
     });
 });
