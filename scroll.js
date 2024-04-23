0545b03c783972d0bd32aa86da478e4499865099
Gabriel Velez <gabe@designoncue.com>
2024-04-23 06:11:14 -0400

    more scroll testing

--- scroll.js
+++ scroll.js
@@ -1,24 +1,8 @@
-$(document).ready(function() {
-
-    console.log("Scroll loaded");
-
-    var $navbar = $(".navbar");
-    console.log("should have read navbar class");
-
-    $(window).on("scroll", function() {
-    console.log("got past window on scroll");
-
-        if ($(this).scrollTop() > 50) {
-            console.log("read if statement");
-
-            $navbar.addClass("nav__active");
-            console.log("class added");
-
-        } else {
-            console.log("read else");
-
-            $navbar.removeClass("nav__active");
-            console.log("class removed");
-        }
-    });
+$(window).on("scroll", function() {
+    console.log("scroll dude");
+    if($(window).scrollTop() > 10) {
+        $(".header").addClass("nav__active");
+    } else {
+       $(".header").removeClass("nav__active");
+    }
 });
