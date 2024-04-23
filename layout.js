8aa5a36e8d571e8bffa7b5e138c9455ebdb0edcf
Gabriel Velez <gabe@designoncue.com>
2024-04-23 04:41:04 -0400

    adding logs to see whats going on

--- layout.js
+++ layout.js
@@ -5,10 +5,17 @@ $(function() {
 
 $(document).ready(function() {
     $(window).on("scroll", function() {
-        if ($(this).scrollTop() > 50) {
+        // Log a message when the scroll event occurs
+        console.log("Scroll event occurred");
+
+        if ($(this).scrollTop() > 20) {
             $(".navbar").addClass("nav__active");
+            // Log a message when the nav__active class is added
+            console.log("nav__active class added");
         } else {
             $(".navbar").removeClass("nav__active");
+            // Log a message when the nav__active class is removed
+            console.log("nav__active class removed");
         }
     });
 });
