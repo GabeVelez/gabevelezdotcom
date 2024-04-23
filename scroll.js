502d306c84ef2162a34e3105927a60f6e30c4d84
Gabriel Velez <gabe@designoncue.com>
2024-04-23 06:03:08 -0400

    still in the scroll weeds

--- scroll.js
+++ scroll.js
@@ -3,21 +3,20 @@ $(document).ready(function() {
     console.log("Scroll loaded");
 
     var $navbar = $(".navbar");
+    console.log("should have read navbar class");
 
-    console.log("should have read navbar classs");
-
-    // Listen for the scroll event on the window
     $(window).on("scroll", function() {
-        console.log("got past window on scroll");
-        // Check if the scroll position is greater than 0 (i.e., scrolled down from the top)
+    console.log("got past window on scroll");
+
         if ($(this).scrollTop() > 50) {
-            console.log("read if");
-            // Add a class to the navbar to change its background color
+            console.log("read if statement");
+
             $navbar.addClass("nav__active");
             console.log("class added");
+
         } else {
             console.log("read else");
-            // Remove the class if the scroll position is at the top
+
             $navbar.removeClass("nav__active");
             console.log("class removed");
         }
