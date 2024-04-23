267b6461902a9c2576b79f2fef07d3386c260496
Gabriel Velez <gabe@designoncue.com>
2024-04-23 06:55:14 -0400

    dunno why jquery giving me issues but its all a process

--- layout.js
+++ layout.js
@@ -1,4 +1,7 @@
 $(function() {
     $("header").load("layout/nav.html");
+    console.log("nav loaded");
     $("footer").load("layout/footer.html");
+    console.log("footer loaded");
+    console.log("hola dude");
 });
--- scroll.js
+++ scroll.js
@@ -1,3 +1,10 @@
-window.onscroll = function() {
-    console.log('scrolling');
-};
+$(function() {
+    $(window).on("scroll", function() {
+        console.log("scroll dude");
+        if($(window).scrollTop() > 10) {
+            $("header").addClass("nav__active");
+        } else {
+           $("header").removeClass("nav__active");
+        }
+    });
+});
