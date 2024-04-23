48493cb9a9df221f76843e9f74915dba08ecf280
Gabriel Velez <gabe@designoncue.com>
2024-04-23 05:47:36 -0400

    really trying to get scroll.js to work

--- scroll.js
+++ scroll.js
@@ -4,16 +4,22 @@ $(document).ready(function() {
 
     var $navbar = $(".navbar");
 
+    console.log("should have read navbar classs");
+
     // Listen for the scroll event on the window
     $(window).on("scroll", function() {
+        console.log("got past window on scroll");
         // Check if the scroll position is greater than 0 (i.e., scrolled down from the top)
-        if ($(this).scrollTop() > 0) {
+        if ($(this).scrollTop() > 50) {
+            console.log("read if");
             // Add a class to the navbar to change its background color
             $navbar.addClass("nav__active");
+            console.log("class added");
         } else {
+            console.log("read else");
             // Remove the class if the scroll position is at the top
             $navbar.removeClass("nav__active");
+            console.log("class removed");
         }
     });
 });
-    
