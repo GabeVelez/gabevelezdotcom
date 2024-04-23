2d333b248f671f257ad66dd948abd209e2158b4c
Gabriel Velez <gabe@designoncue.com>
2024-04-23 05:04:22 -0400

    testing is scroll.js even loading

--- scroll.js
+++ scroll.js
@@ -1,16 +1,5 @@
 $(document).ready(function() {
     $(window).on("scroll", function() {
-        // Log a message when the scroll event occurs
         console.log("Scroll event occurred");
-
-        if ($(this).scrollTop() > 20) {
-            $(".navbar").addClass("nav__active");
-            // Log a message when the nav__active class is added
-            console.log("nav__active class added");
-        } else {
-            $(".navbar").removeClass("nav__active");
-            // Log a message when the nav__active class is removed
-            console.log("nav__active class removed");
-        }
     });
 });
