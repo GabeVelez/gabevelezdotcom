548a8ea3173427fb7dfa09ac78f5f8e83d1ca70b
Gabriel Velez <gabe@designoncue.com>
2024-04-23 04:23:44 -0400

    rewriting js for nav so it works

--- layout.js
+++ layout.js
@@ -3,13 +3,12 @@ $(function() {
     $("footer").load("layout/footer.html");
 });
 
-$(function() {
+$(document).ready(function() {
     $(window).on("scroll", function() {
-        if($(window).scrollTop() > 50) {
+        if ($(this).scrollTop() > 50) {
             $(".navbar").addClass("nav__active");
         } else {
-            //remove the background property so it comes transparent again (defined in your css)
-           $(".navbar").removeClass("nav__active");
+            $(".navbar").removeClass("nav__active");
         }
     });
 });
