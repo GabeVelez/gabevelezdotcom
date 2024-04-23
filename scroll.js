bafda618af0310cab09c7e54e80486a00a90aeef
Gabriel Velez <gabe@designoncue.com>
2024-04-23 06:32:29 -0400

    testing aos

--- scroll.js
+++ scroll.js
@@ -1,8 +1,8 @@
 $(window).on("scroll", function() {
     console.log("scroll dude");
     if($(window).scrollTop() > 10) {
-        $(".header").addClass("nav__active");
+        $("header").addClass("nav__active");
     } else {
-       $(".header").removeClass("nav__active");
+       $("header").removeClass("nav__active");
     }
 });
