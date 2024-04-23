8d0097e0195f658c99768132f971ebb9e2f18f59
Gabriel Velez <gabe@designoncue.com>
2024-04-23 05:14:57 -0400

    bare bones test of scroll.js

--- scroll.js
+++ scroll.js
@@ -1,13 +1,3 @@
 $(function() {
-    $(window).on("scroll", function() {
-        console.log("Scroll loaded");
-        if($(window).scrollTop() > 50) {
-            $(".navbar").addClass("nav__active");
-            console.log("nav__active class added");
-        } else {
-            //remove the background property so it comes transparent again (defined in your css)
-           $(".navbar").removeClass("nav__active");
-           console.log("nav__active class removed");
-        }
-    });
+    console.log("Scroll loaded");   
 });
