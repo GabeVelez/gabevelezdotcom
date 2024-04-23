d04b8f395110dcdccc5c8109dcf49c78cecb47e4
Gabriel Velez <gabe@designoncue.com>
2024-04-23 05:09:20 -0400

    still trying to get scroll js to work

--- scroll.js
+++ scroll.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
+$(function() {
     $(window).on("scroll", function() {
-        console.log("Scroll event occurred");
+        console.log("Scroll loaded");
+        if($(window).scrollTop() > 50) {
+            $(".navbar").addClass("nav__active");
+            console.log("nav__active class added");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+           $(".navbar").removeClass("nav__active");
+           console.log("nav__active class removed");
+        }
     });
 });
