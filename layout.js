8e49f482af46b262b2108377822890c5ba7b7adb
Gabriel Velez <gabe@designoncue.com>
2024-04-23 18:56:32 -0400

    removing console logs from js and adding back overflow to test

--- layout.js
+++ layout.js
@@ -1,7 +1,4 @@
 $(function() {
     $("header").load("layout/nav.html");
-    console.log("nav loaded");
     $("footer").load("layout/footer.html");
-    console.log("footer loaded");
-    console.log("hola dude");
 });
--- scroll.js
+++ scroll.js
@@ -1,13 +1,9 @@
 $(function() {
-    console.log("scroll js file function started");
     $(window).on("scroll", function() {
-        console.log("windo scroll began");
         if($(window).scrollTop() > 10) {
             $("header").addClass("nav__active");
-            console.log("added class");
         } else {
            $("header").removeClass("nav__active");
-           console.log("removed class");
         }
     });
 });
