6da7e0852b2dd2295ac8539ff433feb72fb80460
Gabriel Velez <gabe@designoncue.com>
2024-04-24 18:00:08 -0400

    testing some relative linkage stuff

--- layout.js
+++ layout.js
@@ -1,4 +1,9 @@
 $(function() {
+    if ($("body").hasClass("casestudy")) {
+        $("header").load("../layout/nav.html");
+        $("footer").load("../layout/footer.html");
+    } else {
     $("header").load("layout/nav.html");
     $("footer").load("layout/footer.html");
+}
 });
